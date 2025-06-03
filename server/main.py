0571e1ff75586924cf07ca74b8d6fc7e58777b91
Victor Vidalot <vidalotvictor@gmail.com>
2025-06-03 08:29:47 +0200

    Basic route to HTMLResponse

--- server/main.py
+++ server/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 # Initialize the FastAPI application
 app = FastAPI()
 
 
 # Define the default route
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
     return "<html><body><h1>Welcome to the FastAPI Server!</h1></body></html>"
