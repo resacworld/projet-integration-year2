99a303a5331c7cab791c48ac7d1d2d428f558eb0
lcapaimerir <loic.capa@imerir.com>
2025-06-03 11:12:58 +0200

    Test connectivité avec le serveur

--- simulator/src/main/java/module-info.java
+++ simulator/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.osr.simulator {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires com.fasterxml.jackson.databind;
 
     opens com.osr.simulator to javafx.fxml;
     exports com.osr.simulator;
